0f042896af73d999f4bc4ba8a32c171eab1911d5
joseCarlosAndrade <josecarlos.101@usp.br>
2024-02-05 15:21:03 -0300

    gradient text

--- src/components/UserAuthentication.tsx
+++ src/components/UserAuthentication.tsx
@@ -1,15 +1,19 @@
 import { PasswordField } from "./PasswordField";
-import { Input, InputGroup, InputLeftAddon, Stack, Button, Center, Heading, Box, Text } from "@chakra-ui/react";
+import { Input, InputGroup, InputLeftAddon, Stack, Button, Center, Heading, Box, Text, background } from "@chakra-ui/react";
 import styled from 'styled-components';
 import  React, { useState }  from 'react'
 import { useEffect } from "react";
 import { fetchMyAPI } from "../services/apiSimulator";
 import { asyncLogin } from "../services/login";
 
+import CSS from 'csstype';
 // const StyledHeader = styled.h1`
 //     font-size: 30px;
 
 // `
+
+import './UserAuthentication.css';
+
 
 interface MyDataInterface {
     email : string,
@@ -58,10 +62,27 @@ export const UserAuthentication = () => {
             <Button bg='lightBlue' onClick={()=> {asyncLogin(email)}}>Enviar</Button>
             <Box display='flex' flexDir='column' alignItems='center' justifyContent='center'  >
                 <Text fontSize='20px' fontWeight='700'>Texto retornado Promise:</Text>
-                <Box display='flex' flexDir='column' alignItems='center' justifyContent='center'>
-                    <p>{userData?.email} </p>
-                    <p>{userData?.name}</p>
-                    <p>{userData?.password}</p>
+                <Box 
+                    display='flex' flexDir='column' alignItems='center' justifyContent='center'
+                    color='rgba(222, 222, 222, 1)' 
+                    bgColor='rgba(10,0,230,0.5)'
+                    boxShadow='2px 2px 10px 7px rgba(0, 0, 0, 0.3)'
+                    fontSize='2rem'
+                    fontWeight='700'
+                    background='radial-gradient(circle, rgba(50,86,129,1) 0%, rgba(35,41,80,1) 100%)'
+                    borderRadius='13px'
+                    padding='1rem 3rem'>
+                        {/* conditional rendering: */}
+                    {
+                       ( userData === null || userData === undefined ) ?
+                        <p>Loading..</p> :
+                        <div className="gradient-text">
+                            <p>{userData?.email} </p>
+                            <p>{userData?.name}</p>
+                            <p>{userData?.password}</p>
+                        </div>
+                        
+                    }
                     
                 </Box>
             </Box>
